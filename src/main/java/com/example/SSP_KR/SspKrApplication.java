10f96ca064036c3aa951ca84a7c64b51af79b43a
verke <verke@LAPLAPLAPLAP>
2026-01-13 20:32:22 +0300

    Initial Commit

---
+++ src/main/java/com/example/SSP_KR/SspKrApplication.java
@@ -0,0 +1,13 @@
+package com.example.SSP_KR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SspKrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SspKrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SSP_KR/SspKrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SSP_KR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SspKrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
